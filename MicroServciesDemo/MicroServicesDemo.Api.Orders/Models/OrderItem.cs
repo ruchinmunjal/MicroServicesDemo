35eec021c219a0c29d6e3fd25c92aa64cf6d65fe
Ronnie <teenbhaiacc@gmail.com>
2020-06-13 23:26:32 +0100

    Some minor corrections. Added seed data to test

---
+++ MicroServciesDemo/MicroServicesDemo.Api.Orders/Models/OrderItem.cs
@@ -0,0 +1,13 @@
+﻿namespace MicroServicesDemo.Api.Orders.Models
+{
+    public class OrderItem
+    {
+         public int Id { get; set; }
+
+        public int ProductId { get; set; }        
+
+        public decimal UnitPrice { get; set; }
+
+        public int Quantity { get; set; }
+    }
+}
